a07429620d02bb1282f0f77846373eff6b6215f8
sydney0716 <sydney0716@naver.com>
2024-12-23 21:00:00 +0900

    test2 from intelliJ

--- src/App.java
+++ src/App.java
@@ -1,9 +1,29 @@
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org .jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("!");
-        System.out.println("!");
-        System.out.println("!!!");
+    public static void main(String[] args) {
+        final String crawlingEnterUrl = "https://www.hankyung.com/globalmarket/";
+        Connection conn = Jsoup.connect(crawlingEnterUrl);
+
+        try {
+            Document document = conn.get();
+
+            Elements newsTitleElements = document.getElementsByClass("news-tit");
+            for (int i = 0; i < 30; i++) {
+                final String newsTitle = newsTitleElements.get(i).text();
+
+
+                System.out.println("News: " + i + " " + newsTitle);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
 
     }
 }
