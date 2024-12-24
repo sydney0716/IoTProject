5a2e22d2f51c634594ecf94285de0701d7a4a712
sydney0716 <sydney0716@naver.com>
2024-12-24 20:02:08 +0900

    Web crawling

--- src/App.java
+++ src/App.java
@@ -1,10 +1,10 @@
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org .jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.IOException;
+
+
 
 public class App {
     public static void main(String[] args) {
@@ -15,11 +15,11 @@ public class App {
             Document document = conn.get();
 
             Elements newsTitleElements = document.getElementsByClass("news-tit");
-            for (int i = 0; i < 30; i++) {
-                final String newsTitle = newsTitleElements.get(i).text();
-
-
-                System.out.println("News: " + i + " " + newsTitle);
+            for (int i = 0; i < 36; i++) {
+                if ((i < 11 && i !=5) || i == 14 || i == 16 || i == 18 || i == 20 || i == 23 || i == 25 || i == 27 || i == 29 || i == 31 || i == 33 || i == 35){
+                    final String newsTitle = newsTitleElements.get(i).text();
+                    System.out.println("News: " + i + " " + newsTitle);
+                }
             }
         } catch (IOException e){
             e.printStackTrace();
